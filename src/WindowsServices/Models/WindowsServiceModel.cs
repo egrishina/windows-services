20d5bc82f5a9868445f8f8c798dfd8bc5f504953
Ekaterina Adeeva <ekaterina.adeeva2402@gmail.com>
2022-08-08 15:10:27 +0200

    Create application to manage Windows Services

---
+++ src/WindowsServices/Models/WindowsServiceModel.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WindowsServices.Models;
+
+public class WindowsServiceModel : ObservableObject
+{
+    private string _name;
+    private string _displayName;
+    private string _status;
+    private string _account;
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string DisplayName
+    {
+        get => _displayName;
+        set
+        {
+            _displayName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Status
+    {
+        get => _status;
+        set
+        {
+            _status = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Account
+    {
+        get => _account;
+        set
+        {
+            _account = value;
+            OnPropertyChanged();
+        }
+    }
+}
---
+++ src/WindowsServices/ViewModels/MainViewModel.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management;
+using System.ServiceProcess;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Serilog;
+using WindowsServices.Models;
+
+namespace WindowsServices.ViewModels;
+
+public class MainViewModel : ObservableRecipient
+{
+    private const int TimeoutMs = 500;
+
+    private readonly ILogger _logger = Log.ForContext(typeof(MainViewModel));
+
+    private RelayCommand _startServiceCommand;
+    private RelayCommand _stopServiceCommand;
+    private WindowsServiceModel _selectedService;
+
+    public MainViewModel()
+    {
+        WindowsServices = new ObservableCollection<WindowsServiceModel>();
+        GetServices();
+        SelectedService = WindowsServices.FirstOrDefault();
+    }
+
+    public WindowsServiceModel SelectedService
+    {
+        get => _selectedService;
+        set
+        {
+            _selectedService = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ObservableCollection<WindowsServiceModel> WindowsServices { get; }
+
+    public RelayCommand StartServiceCommand => GetCommand(ref _startServiceCommand, StartService, () => true);
+    public RelayCommand StopServiceCommand => GetCommand(ref _stopServiceCommand, StopService, () => true);
+
+    private static RelayCommand GetCommand(ref RelayCommand existingCommand, Action execute, Func<bool> canExecute)
+    {
+        return existingCommand ??= new RelayCommand(execute, canExecute);
+    }
+
+    private void GetServices()
+    {
+        try
+        {
+            WindowsServices.Clear();
+
+            ServiceController[] services = ServiceController.GetServices();
+            _logger.Information("{@count} Windows Services found.", services.Length);
+
+            foreach (ServiceController service in services)
+            {
+                var userName = string.Empty;
+                var query = new SelectQuery(
+                    $"select name, startname from Win32_Service where name = '{service.ServiceName}'");
+
+                using (var searcher = new ManagementObjectSearcher(query))
+                {
+                    var all = searcher.Get();
+                    foreach (ManagementObject obj in all)
+                    {
+                        userName = obj["startname"]?.ToString();
+                    }
+                }
+
+                var item = new WindowsServiceModel
+                {
+                    Name = service.ServiceName,
+                    DisplayName = service.DisplayName,
+                    Status = service.Status.ToString(),
+                    Account = userName
+                };
+                WindowsServices.Add(item);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "Error occured while retrieving Windows Services.");
+        }
+    }
+
+    private void StartService()
+    {
+        if (SelectedService is null)
+        {
+            return;
+        }
+
+        var service = new ServiceController(SelectedService.Name);
+        if (service.Status == ServiceControllerStatus.Stopped)
+        {
+            try
+            {
+                var timeout = TimeSpan.FromMilliseconds(TimeoutMs);
+                service.Start();
+                service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+                SelectedService.Status = service.Status.ToString();
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.Error(ex, "Could not start the service {@name}", SelectedService.Name);
+            }
+        }
+        else
+        {
+            MessageBox.Show($"Service {SelectedService.Name} is already running.", "Information", MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+    }
+
+    private void StopService()
+    {
+        if (SelectedService is null)
+        {
+            return;
+        }
+
+        var service = new ServiceController(SelectedService.Name);
+        if (service.Status == ServiceControllerStatus.Running)
+        {
+            try
+            {
+                var timeout = TimeSpan.FromMilliseconds(TimeoutMs);
+                service.Stop();
+                service.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+                SelectedService.Status = service.Status.ToString();
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.Error(ex, "Could not stop the service {@name}", SelectedService.Name);
+            }
+        }
+        else
+        {
+            MessageBox.Show($"Service {SelectedService.Name} is already stopped.", "Information", MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+    }
+}
