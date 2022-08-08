8bb8043a4a2a9cc73f0c7f0959222994e5aa6675
Ekaterina Adeeva <ekaterina.adeeva2402@gmail.com>
2022-08-08 15:10:27 +0200

    Create application to manage Windows Services

---
+++ src/WindowsServices/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace WindowsServices
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Debug()
+                .CreateLogger();
+
+            services.AddSingleton(Log.Logger);
+        }
+    }
+}
